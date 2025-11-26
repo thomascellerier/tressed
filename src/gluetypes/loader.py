2956eb57b38b7525e1136a5a03edf6d8b7efd4a0
Thomas Cellerier <thomascellerier@gmail.com>
2025-11-26 16:49:10 +0100

    initial experiments with specializer

--- src/gluetypes/loader.py
+++ src/gluetypes/loader.py
@@ -18,7 +18,8 @@ if TYPE_CHECKING:
 
     from gluetypes.predicates import TypePredicate
 
-    type TypePath = tuple[str | int, ...]
+    type TypePathItem = str | int
+    type TypePath = tuple[TypePathItem, ...]
 
     class LoaderProtocol(Protocol):
         def _load[T](
@@ -58,10 +59,101 @@ def _load_simple_sequence[T](
         )
 
     item_type = args[0]
-    return type_form(
+    return origin(
         loader._load(item, item_type, (*type_path, pos))
         for pos, item in enumerate(value)
     )
+
+
+def _specialize_load_tuple[T](
+    type_form: type[T],
+    type_path: TypePath,
+) -> str | None:
+    """
+    Generate specialized function for given type at given path.
+    TODO: Specializer helper class based on ast + ast.unparse to generate code.
+    """
+    from gluetypes.predicates import get_args
+
+    args = get_args(type_form)
+    assert args is not None
+
+    code = """\
+def __specialized_fn(value, loader):
+"""
+    match args:
+        case []:
+            pass
+        case [arg]:
+            code += """\
+    item_0, = value
+"""
+        case [*args]:
+            code += f"""\
+    {", ".join(f"item_{pos}" for pos in range(len(args)))} = value
+"""
+    code += """\
+    _load = loader._load
+    return (
+"""
+    for pos, arg in enumerate(args):
+        type_path_str = repr((*type_path, pos))
+        if type(arg) is not type:
+            # Failed specialization
+            return None
+        code += f"""\
+        _load(item_{pos}, {arg.__qualname__}, {type_path_str}),
+"""
+    code += """\
+    )
+
+"""
+    return code
+
+
+class SpecializingLoader:
+    def __init__(self, loader, specializer) -> None:
+        self._loader = loader
+        self._specialized_loaders: dict = {}
+        self._specializer = specializer
+        self._threshold: int = 3
+
+    def __call__[T](
+        self,
+        value: Any,
+        type_form: type[T],
+        type_path: TypePath,
+        loader: LoaderProtocol,
+    ) -> T:
+        key = (type_form, type_path)
+        specialized_loader = self._specialized_loaders.get(key)
+        if type(specialized_loader) is type(None) or type(specialized_loader) is int:
+            if type(specialized_loader) is int and specialized_loader > 0:
+                count = specialized_loader + 1
+            else:
+                count = 1
+            self._specialized_loaders[key] = count
+            specialized_loader = None
+
+            # Specialize!
+            if count > self._threshold:
+                specializer_fn_code = self._specializer(*key)
+                if specializer_fn_code is None:
+                    # zero means we can't specialize this
+                    self._specialized_loaders[key] = 0
+                else:
+                    globals_ = {}
+                    locals_ = {}
+
+                    exec(specializer_fn_code, globals_, locals_)
+
+                    specialized_loader = locals_["__specialized_fn"]
+                    self._specialized_loaders[key] = specialized_loader
+
+        if specialized_loader is None:
+            return self._loader(value, type_form, type_path, loader)
+        else:
+            return specialized_loader(value, loader)
 
 
 def _load_tuple[*Ts](
@@ -86,38 +178,50 @@ def _load_tuple[*Ts](
 NoneType = type(None)
 
 
-_simple_scalar_loaders = {
-    bool: _load_simple_scalar,
-    int: _load_simple_scalar,
-    float: _load_simple_scalar,
-    str: _load_simple_scalar,
-    NoneType: _load_simple_scalar,
-}
-_default_type_loaders = _simple_scalar_loaders
+def _default_type_loaders():
+    return {
+        bool: _load_simple_scalar,
+        int: _load_simple_scalar,
+        float: _load_simple_scalar,
+        str: _load_simple_scalar,
+        NoneType: _load_simple_scalar,
+    }
 
-# Note that the order matters as some predicates match several types,
-# put the most specific match first.
-_simple_sequence_mappers = {
-    is_homogeneous_tuple: _load_simple_sequence,
-    is_tuple: _load_tuple,
-    is_list: _load_simple_sequence,
-    is_set: _load_simple_sequence,
-    is_frozenset: _load_simple_sequence,
-}
+
+def _default_type_mappers(specialize: bool):
+    # Note that the order matters as some predicates match several types,
+    # put the most specific match first.
+    return {
+        is_homogeneous_tuple: _load_simple_sequence,
+        is_tuple: SpecializingLoader(_load_tuple, _specialize_load_tuple)
+        if specialize
+        else _load_tuple,
+        is_list: _load_simple_sequence,
+        is_set: _load_simple_sequence,
+        is_frozenset: _load_simple_sequence,
+    }
 
 
 class Loader:
     def __init__(
-        self, type_loaders: Mapping[type, TypeLoaderFn] = _default_type_loaders
+        self,
+        type_loaders: Mapping[type, TypeLoaderFn] | None = None,
+        type_mappers: Mapping[TypePredicate, TypeLoaderFn] | None = None,
+        enable_specialization: bool = False,
     ) -> None:
         # Map a type to its loader
-        self._type_loaders: dict[type, TypeLoaderFn] = (
-            type_loaders if isinstance(type_loaders, dict) else dict(type_loaders)
-        )
+        if type_loaders is None:
+            self._type_loaders: dict[type, TypeLoaderFn] = _default_type_loaders()
+        else:
+            self._type_loaders = dict(type_loaders)
+
         # Mapping of type predicate to a loader
-        self._type_mappers: Mapping[TypePredicate, TypeLoaderFn] = (
-            _simple_sequence_mappers
-        )
+        if type_mappers is None:
+            self._type_mappers: Mapping[TypePredicate, TypeLoaderFn] = (
+                _default_type_mappers(enable_specialization)
+            )
+        else:
+            self._type_mappers = dict(type_mappers)
 
     def _load[T](self, value: Any, type_form: type[T], type_path: TypePath) -> T:
         if (type_loader := self._type_loaders.get(type_form)) is None:
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -66,14 +66,41 @@ def test_load_homogeneous_tuple() -> None:
 
 
 def test_load_tuple() -> None:
-    loader = Loader()
+    loader = Loader(enable_specialization=True)
     assert loader.load([], tuple[()]) == ()
     assert loader.load([1], tuple[int]) == (1,)
     assert loader.load([1, True], tuple[int, bool]) == (1, True)
-    assert loader.load([1, True, 1.1], tuple[int, bool, float]) == (1, True, 1.1)
+    for _ in range(10):
+        assert loader.load([1, True, 1.1], tuple[int, bool, float]) == (1, True, 1.1)
 
     with pytest.raises(GluetypesValueError):
         loader.load([1, True, "foo"], tuple[int, bool, float])
+
+
+def test_specialize_load_tuple() -> None:
+    from gluetypes.loader import _specialize_load_tuple
+
+    code = _specialize_load_tuple(tuple[int, float, str], ("foo", 1))
+    assert (
+        code
+        == """\
+def __specialized_fn(value, loader):
+    item_0, item_1, item_2 = value
+    _load = loader._load
+    return (
+        _load(item_0, int, ('foo', 1, 0)),
+        _load(item_1, float, ('foo', 1, 1)),
+        _load(item_2, str, ('foo', 1, 2)),
+    )
+
+"""
+    )
+    loader = Loader()
+    globals_ = {}
+    locals_ = {}
+    exec(code, globals_, locals_)
+    specialized_fn = locals_["__specialized_fn"]
+    assert specialized_fn([1, 1.1, "foobar"], loader) == (1, 1.1, "foobar")
 
 
 def test_load_unknown_type() -> None:
